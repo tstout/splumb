0aa355f325b84e2dc907d86e7954502cf1a2a3ae
Todd Stout <todd.tstout@gmail.com>
2012-12-29 10:25:37 -0600

    remove dead code

--- net/src/main/java/splumb/net/framing/FrameListener.java
+++
@@ -1,9 +0,0 @@
-package splumb.net.framing;
-
-import splumb.net.nio.Client;
-
-import java.nio.ByteBuffer;
-
-public interface FrameListener {
-    void frameAvailable(Client client, ByteBuffer payload);
-}
